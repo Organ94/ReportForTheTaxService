b5129e9e8d6f94fd173e66a53744ecab85ea4a7c
Алексей Печин <organner@mail.ru>
2022-07-05 22:47:25 +0300

    Creating class Main.java Creating the threads

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final int NUMBER_OF_STORES = 3;
+    private static final int ARRAY_SIZE = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        Shop shop = new Shop();
+
+        List<Thread> shopList = creatingStores(NUMBER_OF_STORES, ARRAY_SIZE, shop);
+        for (Thread s : shopList) {
+            s.join();
+        }
+        System.out.println("Выручка всех магазинов за " + ARRAY_SIZE + " дней: " + shop.atomicLong.get());
+    }
+
+    private static List<Thread> creatingStores(int numberOfStores, int arraySize, Shop shop) {
+        List<Thread> shopList = new ArrayList<>();
+        for (int i = 0; i < numberOfStores; i++) {
+            shopList.add(new Thread(null, () -> shop.Tax(arraySize), "Shop-" + i));
+            shopList.get(i).start();
+        }
+        return shopList;
+    }
+}
